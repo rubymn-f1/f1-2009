b98ee46187763228220f9756d8343851374dd77f
Andy Atkinson <andyatkinson@gmail.com>
2009-02-20 00:25:08 -0600

    adding the tipsy jquery plugin and some team member tooltips, removing the friends section, updating the css, following all team members, adding google analytics script

--- rubymn_f1.rb
+++ rubymn_f1.rb
@@ -17,8 +17,8 @@ get '/' do
   @title = "f1.ruby.mn | Team 'ruby.mn' | F1 Overnight Website Challenge"
  
   @photos = flickr.photos.search(:tags => @flickr_tag, :per_page => 29, :page => 1)
-  @friends_timeline = twitter.timeline_for(:friends) rescue nil
-  @friends = twitter.my(:friends) rescue nil 
+  @friends_timeline = twitter.timeline_for(:friends) rescue []
+  @friends = twitter.my(:friends) rescue [] 
   cache(erb(:index))
 end
  
@@ -36,139 +36,56 @@ __END__
  <meta name="description" content="<%= @title %>" />
  <meta name="keywords" content="ruby, rails, non-profit, overnight website challenge, #webchallege, flickr, twitter, competition, charity" />
  <title><%= @title %></title>
- <style type="text/css">
-   body {
-       margin-top: 2%;
-       margin-left: 20%;
-       margin-right: 20%;
-       padding: 5px;
-       text-align: center;
-       background-color: maroon;
-       font-family: helvetica,arial,clean,sans-serif;
-   }
-   div#wrapper {
-      margin: 1em auto;
-      width: 95%;
-      border: 5px solid #ccc;
-      padding: 1em;
-      background-color: #fff;
-      text-align: left;
-      -moz-border-radius: 5%;
-      -webkit-border-radius: 20px;
-   }
-   div#wrapper .logo {
-     float: left;
-     height: 100px;
-     margin-right: 25px;
-     border: 2px solid #8B0000;
-   }
-   div#wrapper li {
-     margin-top: .5em;
-   }
-   div#members ul {
-     list-style-type: none;
-   }
-   div#header {
-     height: 120px;
-   }
-   div#members ul li {
-     height: 40px;
-     padding-bottom: 20px;
-     border-bottom: 1px solid lightgray;
-   }
-   div#members img {
-     vertical-align:middle;
-     padding: 0 15px 0 0;
-     float: left;
-   }
-   div#sponsors ul {
-     list-style-type: none;
-   }
-   div#sponsors img {
-     border: none;
-   }
-   div#table {
-     padding: 5px;
-   }
-   div#statuses ul {
-     list-style-type:none;
-   }
-   li.status {
-     border-bottom: 1px solid lightgray;
-     padding-bottom: 10px;
-   }
-   li.status img {
-    vertical-align:middle;
-    padding: 0 15px 0 0;
-    float: left;
-    height: 20px;
-    border: none;
-   }
-   div#photos img {
-     border: 1px solid lightgray;
-     margin: 1px;
-   }
-   span.smaller {
-     font-size: small;
-     color:gray;
-   }
-   div#navcontainer {
-     width: 200px;
-   }
-   ul#navlist
-   {
-    margin-left: 0;
-    padding-left: 0;
-    white-space: nowrap;
-   }
-   #navlist li
-   {
-    display: inline;
-    list-style-type: none;
-   }
-   #navlist a { padding: 4px 3px; }
-   #navlist a:link, #navlist a:visited
-   {
-   color: #fff;
-    background-color: maroon;
-    text-decoration: none;
-   }
-   #navlist a:hover
-   {
-    color: #fff;
-    background-color: #8B0000;
-    text-decoration: none;
-   }
- </style>
+ <link rel="stylesheet" media="screen,projection" type="text/css" href="/stylesheets/f1.ruby.mn.css" />
+ <link rel="stylesheet" media="screen,projection" type="text/css" href="/stylesheets/tipsy.css" />
+ <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js'></script>
+ <script type='text/javascript' src='/javascripts/jquery.tipsy.js'></script>
+ <script type='text/javascript'>
+  $(document).ready(function() {
+    $('div#friends a.tipsy').tipsy({gravity: 'n'});
+    $('a.tipsy').click(function() {
+      return false;
+    });
+  });
+ </script>
 </head>
 <body>
  <div id="wrapper">
+   <div id="navcontainer">
+     <ul id="navlist">
+       <li><a href="#statuses">Statuses</a></li>
+       <li><a href="#photos">Photos</a></li>
+       <li><a href="#links">Links</a></li>
+       <li><a href="#sponsors">Sponsors</a></li>
+     </ul>
+   </div>
    <div id='header'>
      <img src="/images/rubymn.gif" alt="<%= @title %>" title="<%= @title %>" class="logo"/>
       <h1 id="title">Team 'ruby.mn'</h1>
-      <div id="navcontainer">
-        <ul id="navlist">
-          <li><a href="#statuses">Statuses</a></li>
-          <li><a href="#photos">Photos</a></li>
-          <li><a href="#members">Team Members</a></li>
-          <li><a href="#links">Links</a></li>
-          <li><a href="#sponsors">Visit our Sponsors!</a></li>
-        </ul>
-      </div>
+      <div id='friends'><% @friends.each do |f| %><a title="<img src='<%= f.profile_image_url %>' alt='<%= f.name %>' title='<%= f.name %>'/><br/><br/><%= f.name %><br/><%= f.url %><br/><%= f.description %>" href='#' class='tipsy'><img src="<%= f.profile_image_url %>" alt="<%= f.name %>"/></a><% end %></div>
    </div>
     <%= yield %>
  </div>
+ 
+ <script type="text/javascript">
+ var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+ document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+ </script>
+ <script type="text/javascript">
+ try {
+ var pageTracker = _gat._getTracker("UA-7564990-1");
+ pageTracker._trackPageview();
+ } catch(err) {}</script>
 </body>
 </html>
  
 @@ index
  
 <div id="statuses">
-  <h3>Twitter statuses</h3>
+  <h3>Current Team</h3>
  <ul>
    <% for status in @friends_timeline %>
-      <li class='status'><a href='<%=
-  "http://twitter.com/#{status.user.screen_name}" %>'><img src="<%= status.user.profile_image_url %>" alt="<%= status.user.name %>" title="<%= status.user.name %>"/> <%=
+      <li class='status'><a href='<%= "http://twitter.com/#{status.user.screen_name}" %>'><img src="<%= status.user.profile_image_url %>" alt="<%= status.user.name %>" title="<%= status.user.name %>"/> <%=
   status.user.screen_name %></a>&nbsp;<%= status.text.gsub(/((https?:\/\/|www\.)([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/, %Q{<a href="\\1">\\1</a>}).gsub(/@(\w+)/, %Q{<a href="http://twitter.com/\\1">@\\1</a>}) %>&nbsp;
         <span class='smaller'><a href='<%= "http://twitter.com/#{status.user.screen_name}/statuses/#{status.id}"%>'><%= status.created_at.strftime("%m/%d/%Y") %></a></span>
       </li>
@@ -183,29 +100,6 @@ __END__
   <% end %>
 </div>
 
-<div id="members">
-  <h3>Current Members on twitter</h3>
-  <ul>
-     <% for friend in @friends %>
-       <li><img src="<%= friend.profile_image_url %>" alt="<%= friend.name %>" title="<%= friend.name %>"/>
-         <a href='<%= "http://twitter.com/#{friend.screen_name}" %>'><%= friend.name %></a>
-         <%= "<br/>Web: <a href='#{friend.url}'>#{friend.url}</a>" if friend.url %>
-       </li>
-     <% end %>
-  </ul>
-</div>
-
-<div id="alumni">
-  <h3>Alumni (2008)</h3>
-  <ul>
-    <li>Lars Klevan</li>
-    <li><a
-  href="http://graphickarma.com/">Alicia Weller</a></li>
-    <li><a
-    href="http://smokejumperit.com/">Robert Fischer</a></li>
-  </ul>
-</div>
-
 <div id="links">
   <h3>Links</h3>
   <ul>
@@ -216,6 +110,16 @@ __END__
     <li>Non-profit organization from 2008: <a href="http://www.littlebrothersmn.org/">Little Brothers of Minnesota</a></li>
     <li><a href="http://github.com/rubymn-f1">Team Ruby.mn on github</a></li>
   </ul>
+  <div id="alumni">
+    <h3>Alumni (2008)</h3>
+    <ul>
+      <li>Lars Klevan</li>
+      <li><a
+    href="http://graphickarma.com/">Alicia Weller</a></li>
+      <li><a
+      href="http://smokejumperit.com/">Robert Fischer</a></li>
+    </ul>
+  </div>
 </div>
 
 <div id="sponsors">
